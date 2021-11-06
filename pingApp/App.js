7328b5a034ffced62e3b2e3ddb197910c7bb3b76
Damien Smith <19ds6@queensu.ca>
2021-11-06 18:07:24 -0400

    html added

--- pingApp/App.js
+++ pingApp/App.js
@@ -1,15 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Button, Alert } from 'react-native';
-
+import { StyleSheet, Text, View, Button, Alert, safeAreaView} from 'react-native';
+import HTMLView from 'react-native-htmlview'
+ 
+const htmlContent = "<p>THIS IS HTML</p>";
 // View -> UIView
 const App = () => {
+  console.log("started on device");
   return (
     <View style={styles.container}>
-      <Button
-        title="PING"
-        onPress={() => Alert.alert('sent')}
-      />
+      <HTMLView value={htmlContent}/>
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -22,8 +23,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  appButtonContainer: {
+    elevation: 8,
+    backgroundColor: "#009688",
+    borderRadius: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 12
+  },
 });
-
-
 
 export default App;
