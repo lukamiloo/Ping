6aa036b7ceeb07f85e0efeb2c094343b3fe18b95
ajmager <61753697+ajmager@users.noreply.github.com>
2021-11-07 10:19:57 -0500

    added location and message database commits

--- pingApp/backend.py
+++ pingApp/backend.py
@@ -5,3 +5,14 @@ def profile(name, phone, address):
     user = User(phone=phone, name=name,address=address)
     db.add(user)
     db.commit()
+
+def location(longitude, latitude, phone):
+    user = User.query.filter_by(phone = phone).all()
+    temp = User(phone = user.phone, longitude = longitude, latitude = latitude)
+    db.add(user)
+    db.commit()
+
+def message(message):
+    user = User(message = message)
+    db.add(user)
+    db.commit()
